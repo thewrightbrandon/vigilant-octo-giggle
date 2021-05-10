231c17150d561ba377d8c24adb0324f7cfe98732
lanegrimaud <lanewgrimaud@gmail.com>
2021-05-10 14:55:38 -0400

    created seed model and route

---
+++ models/movies_seed.js
@@ -0,0 +1,26 @@
+module.exports = [
+    {
+        title: "The Shawshank Redemption",
+        image: "https://www.filmsite.org/posters/shawshankredemption.jpg",
+        year: "1994",
+        genre: "Drama",
+    },
+    {
+        title: "The Dark Knight",
+        image: "https://images-na.ssl-images-amazon.com/images/I/91ebheNmoUL._RI_.jpg",
+        year: "2008",
+        genre: "Action",
+    },
+    {
+        title: "Spirited Away",
+        image: "https://m.media-amazon.com/images/M/MV5BMjlmZmI5MDctNDE2YS00YWE0LWE5ZWItZDBhYWQ0NTcxNWRhXkEyXkFqcGdeQXVyMTMxODk2OTU@._V1_.jpg",
+        year: "2001",
+        genre: "Adventure",
+    },
+    {
+        title: "Whiplash",
+        image: "https://miro.medium.com/max/2800/1*QtaiJr0QvMOAEfr69gEnBg.jpeg",
+        year: "2014",
+        genre: "Drama",
+    },
+];
