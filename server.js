dd21eba16aa947ba35f5ba758bce070e6937d533
lanegrimaud <lanewgrimaud@gmail.com>
2021-05-10 13:57:11 -0400

    set up server.js and .env, and installed dotenv and mongoose

--- server.js
+++ server.js
@@ -1,10 +1,47 @@
+///////////////////////
+// Dependencies
+///////////////////////
 const express = require('express');
-const PORT = 3000
-const app = express();
+const mongoose = require('mongoose')
+const moviesController = require('./controllers/movies_controller.js');
 
-app.get('/', (req, res) => {
-    res.send('Hello World')
-});
+///////////////////////
+// Config
+///////////////////////
+const app = express();
+require('dotenv').config();
+const MONGODB_URI = process.env.MONGODB_URI
+const PORT = process.env.PORT;
+
+///////////////////////
+// Middleware
+///////////////////////
+app.use(express.json())
+app.use(express.static('public'))
+
+///////////////////////
+// Controllers
+///////////////////////
+app.use('/movies', moviesController)
+
+///////////////////////
+// Routes
+///////////////////////
+
+///////////////////////
+// Connections
+///////////////////////
+mongoose.connect(MONGODB_URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useFindAndModify: false
+})
+
+mongoose.connection.on('error', err =>
+    console.log(err.message, ' is Mongod not running?/Problem with Atlas Connection?'))
+mongoose.connection.on('connected', () =>
+    console.log('mongo connected: ', MONGODB_URI))
+mongoose.connection.on('disconnected', () => console.log('mongo disconnected'))
 
 
 ///////////////////////
@@ -13,4 +50,3 @@ app.get('/', (req, res) => {
 app.listen(PORT, () => {
     console.log('listening on port', PORT);
 });
-
