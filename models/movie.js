70bca07aa4f042d47ff6fd2f9f0a64409937da2c
lanegrimaud <lanewgrimaud@gmail.com>
2021-05-10 14:07:10 -0400

    fixed movie schema

--- models/movie.js
+++ models/movie.js
@@ -7,10 +7,10 @@ const mongoose = require('mongoose')
 // Schema
 ///////////////////////
 const movieSchema = new mongoose.Schema({
-    name: { Type: String, required: true },
-    image: { Type: String, required: true },
-    year: { Type: Number, required: true },
-    genre: { Type: String, required: true },
+    name: { type: String, required: true },
+    image: { type: String, required: true },
+    year: { type: Number, required: true },
+    genre: { type: String, required: true },
 });
 
 const Movie = mongoose.model('Movie', movieSchema)
